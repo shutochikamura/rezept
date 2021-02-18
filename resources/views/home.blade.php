cb6e10e2534859f41fd3c70c2242f8569ec7b864
shutochikamura <scmarudekaze@gmail.com>
2021-02-18 15:02:43 +0000

    微調整　レシピ一覧に自分の名前

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -62,7 +62,7 @@
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('ホストのレシピ参照') }}
+                                        {{ __('レシピ参照') }}
                                     </button>
                                 </div>
                             </div>
